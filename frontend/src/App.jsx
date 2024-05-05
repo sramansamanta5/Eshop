8d6b2aed53b89dc84322630322c6e2fa0828800c
sraman samanta <sramansamanta2023@gmail.com>
2024-05-05 09:34:10 +0530

    Created a react app and installed Tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='text-red-500 font-bold text-2xl'>ESHOP</h1>
+    </>
+  )
+}
+
+export default App
